66657d489db19fb43449d67af4af1bc11f5d6313
Piotr <p.j.paszko@gmail.com>
2016-12-16 00:10:12 +0100

    Update 2d array.cpp

--- Informatyka/2d array.cpp
+++ Informatyka/2d array.cpp
@@ -3,19 +3,11 @@ using namespace std;
 
 int main() 
 {
-    
-   
     int m= 0, n= 0;
-
     double **T = new double*[m];
     for(int i = 0; i < m; ++i) T[i] = new double[n];
     
-    
-    
     for(int i = 0; i < m; ++i) delete [] T[m];
-    delete [] T;
-   
-    
-       
+    delete [] T;      
 return 0;
 }
