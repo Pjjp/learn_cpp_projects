4ead509727b74d983d4cedf982b0f198358f9c94
Piotr <p.j.paszko@gmail.com>
2017-06-07 13:34:01 +0200

    Update Temperatures.cpp

--- coding_game/Temperatures.cpp
+++ coding_game/Temperatures.cpp
@@ -1,55 +1,58 @@
-#include <iostream>
+##include <iostream>
 #include <string>
-#include <cmath>
 
 using namespace std;
 
-bool namber_or_no(char *znak)
-{
-  bool a = false;
+bool namber_or_no(char znak) {
+	bool a = false;
 
-  for(int i=43; i<58; i++)
-  {
-    if( *znak == (char) i ) a = true;
-  }
-  if( a == true ) return true;
-  else return false;
+	for (int i = 48; i < 58; i++) {
+		if (znak == (char) i)
+			a = true;
+	}
+	if ((znak == (char) 43) || (znak == (char) 45)) {
+		a = true;
+	}
+	if (a == true)
+		return true;
+	else
+		return false;
 }
 
-int main()
-{
-  int x = 0;
-  string liczby = " 243 4 1 12";
-  string *str_arr = new string [liczby.length()];
+int main() {
+	int x = 0, y = 0, nr = 0;
+	string liczby = " 243 4 1 12";
+	string strTab[liczby.length()];
+	int *iLiczby = new int[liczby.length()];
+	int *startILiczby = &iLiczby[0];
 
-  char *p_licz = &liczby[0];
-  char *p_arr = &str_arr[x][0];
+	for (int var1 = 0; var1 < liczby.length()+1; ++var1) {
+		if (namber_or_no(liczby[var1]) == true && liczby[var1-1]==(char)32) {
+			x = var1;
+		}
+		if (namber_or_no(liczby[var1]) != true){
+			y=var1;
+			strTab[nr] = liczby.substr(x, y-x);
+			nr++;
+		}
+		if(var1==NULL){
+			y=var1;
+			strTab[nr] = liczby.substr(x, y-x);
+		}
+	}
+	for (int var = 0; var < liczby.length(); ++var) {
+		if(strTab[var].empty()==false){
+			*iLiczby=stoi(strTab[var]);
+			iLiczby++;
+		}
+	}
+	iLiczby=startILiczby;
+	while(*iLiczby){
+		cout << *iLiczby << endl;
+		iLiczby++;
+	}
 
-  while(*p_licz)
-  {
-    while( namber_or_no(p_licz) == true )
-    {
-      *p_arr = *p_licz;
-      p_arr++; p_licz++;
-    }
-    p_arr = &str_arr[++x][0];
-    p_licz++;
-  }
-	
-  p_arr = &str_arr[0][0];
-
-  while(*p_arr)
-  {
-    cout <<  *p_arr;
-    p_arr++;
-  }
-
-  cout << endl << endl;
-  
-  delete [] str_arr;
-  p_licz = NULL;
-  p_arr = NULL;
-  
-  cin.get();
-  return 0;
+	delete iLiczby;
+	cin.get();
+	return 0;
 }
